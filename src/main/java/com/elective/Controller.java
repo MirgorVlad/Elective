7aaf3d75058a0d9d5eb73101b65428cfbc15b005
MirgorVlad <mirgorodskiy295@gmail.com>
2022-09-25 13:53:17 +0300

    Add simple error collector and error page

--- src/main/java/com/elective/Controller.java
+++ src/main/java/com/elective/Controller.java
@@ -14,21 +14,32 @@ import java.io.IOException;
 @WebServlet("/controller")
 public class Controller extends HttpServlet {
 
+    private static final String ERROR_PAGE = "error_page.jsp";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        //add exception handle
-        String address = getAndExecuteCommand(req, resp);
+        String address = ERROR_PAGE;
+        try {
+            address = getAndExecuteCommand(req, resp);
+        } catch (Exception ex){
+            req.setAttribute("ex", ex);
+            //log
+        }
         req.getRequestDispatcher(address).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
-        //add exception handle
-        String address = getAndExecuteCommand(req, resp);
+        String address = ERROR_PAGE;
+        try {
+            address = getAndExecuteCommand(req, resp);
+        } catch (Exception ex){
+            req.getSession().setAttribute("ex", ex);
+            //log
+        }
         resp.sendRedirect(address);
     }
 
--- src/main/webapp/error_page.jsp
+++ src/main/webapp/error_page.jsp
@@ -1,6 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>Hello World!</h2>
+     Error
+
 </body>
 </html>
